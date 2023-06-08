96a27a68bc7ef377e6268fd36fccd76907e7f6f2
Unc <135977564+Uncsypn@users.noreply.github.com>
2023-06-09 02:20:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-// This script is used to dynamically load the content for each tab
-document.addEventListener('DOMContentLoaded', function() {
-  var contentDiv = document.getElementById('content');
-
-  // Retrieve the current URL to determine the active tab
-  var path = window.location.pathname;
-  var page = path.split("/").pop();
-
-  // Load the appropriate tab content based on the active tab
-  if (page === 'home.html') {
-    contentDiv.innerHTML = '<object type="text/html" data="home.html" />';
-  } else if (page === 'download.html') {
-    contentDiv.innerHTML = '<object type="text/html" data="download.html" />';
-  } else {
-    contentDiv.innerHTML = '<object type="text/html" data="home.html" />';
-  }
-});
+// Empty for now, you can add your own scripts here if needed
